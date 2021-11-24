9a3f78465e7b5741ac0cd05a4878980bcb7eff2a
galacticglum <nerftactic09@gmail.com>
2021-11-24 14:04:28 -0500

    Started empty tile drawing

--- frogger.asm
+++ frogger.asm
@@ -12,12 +12,53 @@
 init:
 	lw $t0, base_display_addr		# $t0 stores the base address for display
 	lw $t1, max_display_addr		# $t1 stores the max address for display
+	li $t2, 0xffffff			# $t2 stores the colour code for white
 game_loop:
 	jal clear_screen
 	jal draw
 	jal wait_frame
 	j game_loop
 draw:
+	li $a0, 1
+	li $a1, 1
+	jal draw_empty_tile
+	
+	li $a0, 2
+	li $a1, 2
+	jal draw_empty_tile
+	
+	jr $ra
+draw_empty_tile:
+	# Draws an 8x8 empty tile 
+	# Arguments:
+	#  - $a0 stores the TOP LEFT X coordinate
+	#  - $a1 stores the TOP LEFT Y coordinate
+	#
+	addi $t7, $ra, 0	# save the return address of the caller
+	jal pixel_coords_to_memory_addr
+	addi $ra, $t7, 0	# restore the return address of the caller
+	# draw the tile
+	sw $t2, 0($v0)
+	jr $ra
+pixel_coords_to_memory_addr:
+	# Get the memory address corresponding to the given pixel coordinates.
+	# Arguments:
+	#  - $a0 stores the X coordinate
+	#  - $a1 stores the Y coordinate
+	# Returns: the memory address stored in $v0
+	#
+	# translate tile coordinates into pixel coordinates
+	sll $a0, $a0, 3	# multiply $a0 by 8 (tile width)
+	sll $a1, $a1, 3	# multiply $a1 by 8 (tile height)
+
+	# translate pixel coordinates to address space
+	# $t8 stores the top left coordinate of the tile in address space
+	addi $v0, $a1, 0
+	sll $v0, $v0, 6		# mutliply $t8 by 512 / 8 = 64 (shift by 6 = log2(64) positions)
+	add $v0, $v0, $a0	# add horizontal offset $a0 to $t8
+	sll $v0, $v0, 2		# multiply $t8 by 4 to get memory address of top left coordinate
+	add $v0, $v0, $t0	# offset $t8 by base_display_addr
+	
 	jr $ra
 wait_frame:
 	li $v0, 32				# load 32 into $v0 to specify that we want the sleep syscall
